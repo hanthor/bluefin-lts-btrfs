c8db980d148f04c7a629d3ebca2bc908f96e871c
James Reilly <jreilly1821@gmail.com>
2025-02-04 15:53:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,8 +10,13 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+dnf install -y btrfs-progs centos-release-hyperscale centos-release-hyperscale-kernel
 
+dnf update -y
+
+dnf clean all
+
+echo -e '[install]\nroot-fs-type = "btrfs"' > /usr/lib/bootc/install/20-rhel.toml
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -21,4 +26,3 @@ dnf install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
